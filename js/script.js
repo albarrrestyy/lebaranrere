12245247e9d94387c1e5e7a26e4ce0fc6c219d26
albarrrestyy <albar.vigriawan@gmail.com>
2025-03-31 03:43:49 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ let slideIndex = 0;
 function showSlides() {
     let slides = document.getElementsByClassName("slide");
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display = "none"; // Sembunyikan semua slide
     }
     slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000); // Ganti gambar setiap 3 detik
+    if (slideIndex > slides.length) { slideIndex = 1; } // Kembali ke slide pertama jika sudah habis
+    slides[slideIndex - 1].style.display = "block"; // Tampilkan slide yang aktif
+    setTimeout(showSlides, 3000); // Ganti slide setiap 3 detik
 }
